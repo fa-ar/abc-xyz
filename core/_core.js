c4ec3d5eeaad7a14c2fd6cb766d876f9ad2ed9bb
fa-ar <97816972+fa-ar@users.noreply.github.com>
2022-09-27 11:56:20 -0600

    added create, render and add core fucntions

--- core/_core.js
+++ core/_core.js
@@ -1,7 +1,7 @@
 /// Imports
+import { determineNodeType } from './_utils.js'
 
-
-///
+/// creator function
 function create ( tag, { attributes={}, children=[] } ) {
 
     // create element
@@ -15,6 +15,44 @@ function create ( tag, { attributes={}, children=[] } ) {
 
     } )
 
+    return virtualElement
+
 }
 
-///
+/// rendering function
+function render ( { tag, attributes, children } ) {
+
+    // create the actual DOM element
+    const element = document.createElement ( tag )
+
+    // add all the attributes specified in the virtualNode.attributes
+    for ( const [ key, value ] of Object.entries ( attributes ) ) {
+
+        element.setAttribute ( key, value )
+
+    }
+
+    // append all the children
+    for ( const child of children ) {
+
+        element.appendChild ( determineNodeType ( child ) )
+
+    }
+
+    return element
+
+}
+
+
+/// 
+function add ( virtualNode, container ) {
+
+    container.replaceWith ( virtualNode )
+
+    return virtualNode
+
+}
+
+/// Exports
+
+export { create, render, add }
